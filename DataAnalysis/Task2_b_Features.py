eef98a7dddc5afbcbaeb5c9a676ecf4e4330c900
Bala Subrahmanya Ravikanth Vara <BSRV1987@GMAIL.COM>
2024-03-19 15:33:08 +0000

    Features added to the users

---
+++ DataAnalysis/Task2_b_Features.py
@@ -0,0 +1,21 @@
+import duckdb
+import os
+import pandas as pd
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+duckdb_file_path = os.path.join(current_dir, '..', 'DataIngestion', 'ListenBrainz.db')
+
+
+def CreateConnection():
+    con = duckdb.connect(duckdb_file_path)
+    return con
+
+
+ddb_con = CreateConnection()
+
+result = ddb_con.sql("Select user_name As user_id,track_name,artist_name,release_name, to_timestamp(listened_at) As Listened_At,listening_from from Tracks").to_df()
+
+df_features = pd.DataFrame(result)
+
+print(df_features)
